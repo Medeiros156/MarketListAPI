351703ac50a570f5ac323c72d4cc89c43082b951
Medeiros156 <101615020+Medeiros156@users.noreply.github.com>
2023-03-13 17:55:56 -0300

    catch fix

--- models/apiImage.js
+++ models/apiImage.js
@@ -1,24 +1,27 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-var IMAGEAUTH =  process.env.IMAGEAUTH;
+var IMAGEAUTH = process.env.IMAGEAUTH;
 
 export const fetchUnsplashImage = async (keyWord) => {
-  const response = await fetch(
-    `https://api.unsplash.com/search/photos?page=10&query=${keyWord}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: IMAGEAUTH,
-      },
-      mode: "cors",
-      cache: "no-cache",
-      credentials: "same-origin",
-      redirect: "follow",
-      referrerPolicy: "no-referrer",
-    }
-  );
-
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/search/photos?page=10&query=${keyWord}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: IMAGEAUTH,
+        },
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
